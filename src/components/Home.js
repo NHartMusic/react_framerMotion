977b1d43df806345418742e63158616ae0a97692
Nick Hart <nhartmusic@gmail.com>
2020-07-01 14:07:51 +0200

    simple animation from initial state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
+    <motion.div className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <h3>Welcome to </h3>
+      <motion.h2 animate={{ fontSize: 50, color: '#ff2994' }}>
+        The BONE CLONE ZONE
+      </motion.h2>
       <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
+        <motion.button
+          animate={{ scale: 0.7 }}
+        >
+          What Kinds of Bone Clones Do You Like to <motion.span animate={{ color: '#ff2994' }}>PLAY</motion.span> With?
+        </motion.button>
       </Link>
-    </div>
+    </motion.div>
   )
 }
 
-export default Home;
+export default Home
